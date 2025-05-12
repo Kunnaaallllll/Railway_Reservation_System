ac1d0a620a2dbd816d13eff7e27e77a2c6a7c9da
Kunnaaallllll <kunalchaudhary.cvf@gmail.com>
2025-05-13 02:17:09 +0530

    Delete passenger-service directory

--- passenger-service/src/main/java/com/example/admin_service/service/AdminServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.admin_service.service;
-
-public interface AdminServiceInterface {
-
-	String updateTrainName(String name, String newName);
-	String updateDepartureStation(String TrainNo, String station);
-	String updateArrivalStation(String TrainNo, String station);
-	String updateFare(String TrainNo, double fare);
-}
